745d558c23eaed2100f8eb453e3579b511c0cc50
forEngineer-hub <70783654+forEngineer-hub@users.noreply.github.com>
2022-07-19 11:11:07 +0900

    todos sample

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
-import { useState, useEffect } from 'react';
-
-import CardList from './components/card-list/card-list.component';
-import SearchBox from './components/search-box/search-box.component';
-import './App.css';
-
+import { useState, useEffect, useRef } from "react";
+import axios from "axios";
 const App = () => {
-  const [searchField, setSearchField] = useState('');
-  const [monsters, setMonsters] = useState([]);
-  const [filteredMonsters, setFilterMonsters] = useState(monsters);
-
+  const taskElement = useRef();
+  const dateElement = useRef();
+  let id = useRef(3);
+  const [todos, setTodos] = useState([]);
+  
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => setMonsters(users));
+    axios.get("http://localhost:8081/todos").then(({data}) => {
+      console.log(data);
+      setTodos(data.data)
+    });
   }, []);
 
-  useEffect(() => {
-    const newFilteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
+  const addTask = () => {
+    const task = taskElement.current.value;
+    const date = dateElement.current.value;
+    id.current++;
+    let _todos = [...todos]; //copy state
+    console.log(id.current);
+    _todos.push({ task, date, status: 0, id: id.current });
+    setTodos(_todos);
+  };
 
-    setFilterMonsters(newFilteredMonsters);
-  }, [monsters, searchField]);
-
-  const onSearchChange = (event) => {
-    const searchFieldString = event.target.value.toLocaleLowerCase();
-    setSearchField(searchFieldString);
+  const removeTask = (id) => {
+    let _todos = [...todos]; //copy state
+    const filteredTodos = _todos.filter((todo) => todo.id !== id);
+    setTodos(filteredTodos);
   };
 
   return (
-    <div className='App'>
-      <h1 className='app-title'>Monsters Rolodex</h1>
-
-      <SearchBox
-        className='monsters-search-box'
-        onChangeHandler={onSearchChange}
-        placeholder='search monsters'
-      />
-      <CardList monsters={filteredMonsters} />
+    <div className="App">
+      <div>
+        <input ref={taskElement} />
+        <input ref={dateElement} />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <button>sort by time</button>
+      <div>
+        {todos.map(({ id, task, date, status }) => {
+          //console.log("id is **************** ", id);
+          return (
+            <div key={id}>
+              <span>{task}</span>
+              <button>{date}</button>
+              <button onClick={() => removeTask(id)}>remove</button>
+              <button>toggle</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
